dfd15293e5b1cc27f88bf7b3953aa2c6af27047a
Shota HAMADA <maku3ts@gmail.com>
2018-07-31 00:59:48 +0900

    Restructure the project

--- Dx11ComputeParticle/App.cpp
+++ Dx11ComputeParticle/App.cpp
@@ -1,11 +1,14 @@
 #include "pch.h"
 
 using namespace winrt;
-
 using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel;
 using namespace Windows::UI::Core;
+using namespace Windows::Storage::Streams;
+using namespace Windows::Storage;
 
-struct App : implements<App, IFrameworkViewSource, IFrameworkView> {
+class App : public implements<App, IFrameworkViewSource, IFrameworkView> {
+public:
   IFrameworkView CreateView() { return *this; }
 
   void Initialize(const CoreApplicationView &) {}
@@ -23,6 +26,26 @@ struct App : implements<App, IFrameworkViewSource, IFrameworkView> {
           CoreProcessEventsOption::ProcessAllIfPresent);
     }
   }
+
+private:
+  std::vector<uint8_t> ReadFile(const hstring &name) {
+    const auto root = Package::Current().InstalledLocation();
+    const auto file = root.GetFileAsync(name).get();
+    const auto buf = FileIO::ReadBufferAsync(file).get();
+    std::vector<uint8_t> data(buf.Length());
+    DataReader::FromBuffer(buf).ReadBytes(data);
+    return data;
+  }
+
+   com_ptr<ID3D11Device> device{};
+   com_ptr<ID3D11DeviceContext> context{};
+   com_ptr<IDXGISwapChain1> swapchain{};
+   com_ptr<ID3D11RenderTargetView> rtv{};
+   com_ptr<ID3D11DepthStencilView> dsv{};
+   com_ptr<ID3D11VertexShader> vertexShader{};
+   com_ptr<ID3D11PixelShader> pixelShader{};
+   com_ptr<ID3D11InputLayout> inputLayout{};
+   com_ptr<ID3D11Buffer> vertexBuffer{};
 };
 
 int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int) {
--- Dx11ComputeParticle/pch.h
+++ Dx11ComputeParticle/pch.h
@@ -7,8 +7,14 @@
 
 #define NOMINMAX
 
-#include <winrt/Windows.ApplicationModel.Core.h>
-#include <winrt/Windows.UI.Core.h>
-
 #include <d3d11_4.h>
 #include <dxgi1_6.h>
+
+#include <winrt/base.h>
+#include <winrt/Windows.ApplicationModel.Core.h>
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.UI.Core.h>
+
+#include <vector>
